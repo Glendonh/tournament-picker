ff8d980bdbb950b6bb9377d3ca97a803ec2cd22b
Glendon Hardin <glendon.hardin@gmail.com>
2024-07-25 00:04:05 -0700

    tournament summary improvements

--- pages/tournamentDetails.tsx
+++ pages/tournamentDetails.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Head from 'next/head'
 import { Collapse } from 'react-collapse'
 import { useForm } from 'react-hook-form'
 import { CompleteTournament, PickemFormVals } from '../types'
@@ -34,6 +35,10 @@ const TournamentDetails = () => {
 
   return (
     <div className="container px-3">
+      <Head>
+        <title>Tournament Details</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <p className="text-3xl text-center">{format.tournamentName}</p>
       <button className="p-3 border border-black rounded-md" onClick={toggleShowEdit}>
         {showEdit ? 'Hide' : 'Show'}
@@ -49,13 +54,17 @@ const TournamentDetails = () => {
                 </CollapseSection>
               </div>
             ))}
-            <SeedsSection control={control} participants={participants} currentSeeds={currentSeeds} />
-            <BracketSection control={control} bracket={bracket} matchDetails={matchDetails} picks={bracketPicks} />
-            <button className="border border-black rounded-md p-3" type="submit">
+            <CollapseSection title="Seeds">
+              <SeedsSection control={control} participants={participants} currentSeeds={currentSeeds} />
+            </CollapseSection>
+            <CollapseSection title="Bracket">
+              <BracketSection control={control} bracket={bracket} matchDetails={matchDetails} picks={bracketPicks} />
+            </CollapseSection>
+            <button className="border border-black rounded-md p-3 mt-3" type="submit">
               SUBMIT
             </button>
           </form>
-          <ul>
+          <ul className="list-inside list-disc">
             <li>update results on a nightly basis</li>
             <li>update seeds and brackets after nights are complete</li>
             <li>store results</li>
@@ -69,7 +78,7 @@ const TournamentDetails = () => {
           <div className="flex flex-row">
             {participants.allParticipants.map((block) => (
               <div key={block.blockName} className="mx-2">
-                <p className="text-xl">{block.blockName}</p>
+                <p className="text-l font-semibold">{block.blockName}</p>
                 {block.blockParticipants.map((p) => (
                   <p key={p.name}>{p.name}</p>
                 ))}
@@ -85,7 +94,7 @@ const TournamentDetails = () => {
                   {n.matches.map((match, mIndex) => {
                     const matchWinner = results?.nights[nIndex]?.matches[mIndex]?.winner
                     return (
-                      <div key={`N${nIndex}M${mIndex}`}>
+                      <div key={`N${nIndex}M${mIndex}`} className={matchWinner === 'draw' ? 'bg-slate-400' : ''}>
                         {`${mIndex + 1}. `}
                         <span className={match.wrestler1 === matchWinner ? 'bg-green-500' : ''}>{match.wrestler1}</span>
                         {` vs `}
@@ -98,8 +107,23 @@ const TournamentDetails = () => {
             </div>
           ))}
           <div>
+            <p className="text-xl">Seeds</p>
+            <div className="flex flex-row">
+              {currentSeeds.map((block) => (
+                <div key={block.blockName + 'seeds'} className="ml-3">
+                  <p className="text-l font-semibold">{block.blockName}</p>
+                  {block.seeds.map((seed, i) => (
+                    <p key={`${block.blockName}seed${i}`}>{`${i + 1}. ${seed.name}`}</p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
             <p className="text-xl">Bracket</p>
-            {`${format.numberAdvancing} wrestler bracket`}
+            {matchDetails.map((details, i) => (
+              <div key={details.label}>{`${i + 1}. ${details.label}`}</div>
+            ))}
           </div>
         </div>
       )}
